d8595f1a13131bcb1059491c14a09ed25328d1fa
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-04-05 01:50:05 +0200

    Create quote.js

---
+++ netlify/functions/quote.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+  const category = event.queryStringParameters.city || 'london';
+
+  try {
+    const response = await axios.get('https://api.api-ninjas.com/v1/quotes', {
+      headers: {
+        'X-Api-Key': 'ax8Fu/Oy6Qb8c8wj8aDyJQ==usUKVIG79ZIIe12I'
+      },
+      params: {
+        category: category
+      }
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
