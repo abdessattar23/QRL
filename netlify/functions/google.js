17a0758069d88f3b75093046a6f31f5ce7d65e18
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-04-05 20:44:49 +0200

    Update google.js

--- netlify/functions/google.js
+++ netlify/functions/google.js
@@ -4,7 +4,7 @@ exports.handler = async (event, context) => {
   const query2 = event.queryStringParameters.query || 'javascript';
   const limit = event.queryStringParameters.limit || 10;
   const filetype = event.queryStringParameters.filetype;
-  const query = 'filetype:' + filetype + '' + query2;
+  const query = 'filetype:${filetype} ' + query2;
   try {
     const results = await googleIt({
       query: query,
