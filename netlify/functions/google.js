77cf43d029e6b6042090de836408a88d2f965365
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-04-05 20:39:42 +0200

    Create google.js

---
+++ netlify/functions/google.js
@@ -0,0 +1,23 @@
+const googleIt = require('google-it');
+
+exports.handler = async (event, context) => {
+  const query2 = event.queryStringParameters.query || 'javascript';
+  const limit = event.queryStringParameters.limit || 10;
+  const filetype = event.queryStringParameters.filetype;
+  const query = 'filetype:' + filetype + '' + query2;
+  try {
+    const results = await googleIt({
+      query: query,
+      limit: limit
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
