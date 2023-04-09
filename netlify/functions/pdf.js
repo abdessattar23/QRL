7450addff01f196f911501aebbc01b371044e70f
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-04-09 16:36:23 +0200

    '

--- netlify/functions/pdf.js
+++ netlify/functions/pdf.js
@@ -1,23 +1,23 @@
-const puppeteer = require('puppeteer-core');
+const axios = require('axios');
 
 exports.handler = async function (event, context) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  const url = event.queryStringParameters.url || 'https://wikiweather.netlify.app';
+  const pdfUrl = `https://chrome.browserless.io/print?pdf.scale=0.8&pdf.landscape=true&url=${encodeURIComponent(url)}`;
+  const response = await axios.get(pdfUrl, {
+    responseType: 'arraybuffer',
+    headers: {
+      'x-api-key': '204c1021-4d40-4350-9b09-78b0c8cfb7f7',
+    },
+  });
+  const pdfBuffer = Buffer.from(response.data);
 
-  try {
-    const url = event.queryStringParameters.url || 'https://wikiweather.netlify.app';
-    await page.goto(url);
-    const pdfBuffer = await page.pdf({ format: 'Letter' });
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment;filename="converted.pdf"',
-      },
-      body: pdfBuffer.toString('base64'),
-      isBase64Encoded: true,
-    };
-  } finally {
-    await browser.close();
-  }
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment;filename="converted.pdf"',
+    },
+    body: pdfBuffer.toString('base64'),
+    isBase64Encoded: true,
+  };
 };
