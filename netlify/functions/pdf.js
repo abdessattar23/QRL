4dab8e9c8a2607a7c1715fb96ffdc32d989bf664
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-04-09 16:44:53 +0200

    '

--- netlify/functions/pdf.js
+++ netlify/functions/pdf.js
@@ -1,23 +1,33 @@
-const axios = require('axios');
+const pdfmake = require('pdfmake');
 
 exports.handler = async function (event, context) {
-  const url = event.queryStringParameters.url || 'https://wikiweather.netlify.app';
-  const pdfUrl = `https://chrome.browserless.io/print?pdf.scale=0.8&pdf.landscape=true&url=${encodeURIComponent(url)}`;
-  const response = await axios.get(pdfUrl, {
-    responseType: 'arraybuffer',
-    headers: {
-      'x-api-key': '204c1021-4d40-4350-9b09-78b0c8cfb7f7',
-    },
-  });
-  const pdfBuffer = Buffer.from(response.data);
+  const url = event.queryStringParameters.url || 'https://r-watch.netlify.app';
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': 'attachment;filename="converted.pdf"',
+  const docDefinition = {
+    content: [
+      { text: 'PDF from URL', style: 'header' },
+      { text: url, link: url },
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+      },
     },
-    body: pdfBuffer.toString('base64'),
-    isBase64Encoded: true,
   };
+
+  const pdf = pdfmake.createPdf(docDefinition);
+
+  return new Promise((resolve, reject) => {
+    pdf.getBuffer((buffer) => {
+      resolve({
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/pdf',
+        },
+        body: buffer.toString('base64'),
+        isBase64Encoded: true,
+      });
+    });
+  });
 };
